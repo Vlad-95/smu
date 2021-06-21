b3d07e22a4889c0423a676d3a357b7bb64dd98eb
vlad_z <vzv@siluetstudio.com>
2021-06-21 13:10:40 +0300

    mainpage

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -8,7 +8,6 @@ const mainSlider = new Swiper('.mainslider', {
     direction: 'vertical',
     loop: false,
     mousewheel: true,
-    autoHeight: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -19,28 +18,68 @@ const mainSlider = new Swiper('.mainslider', {
         scrollbar: {
             el: ".swiper-scrollbar",
         },
-        mousewheel: true,
     },
     
     
 });
 
-//слайдер внутри слайдера
-const smallMainSlider = new Swiper('.mainslider .slider', {
-    // Optional parameters
-    effect: 'fade',
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: false,
-        type: 'bullets',
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
+mainSlider.on('slideChangeTransitionEnd', function () {
+    var acs = document.querySelectorAll('.swiper-slide-active')[0];
+    var hasVerticalScrollbar = acs.scrollHeight > acs.clientHeight;
+    console.log(hasVerticalScrollbar);
+            
+    if (hasVerticalScrollbar) {
+        var scrollHeight = acs.scrollHeight;
+        var slideSize = acs.swiperSlideSize;
+        var scrollDifferenceTop = scrollHeight - slideSize;
+
+        acs.addEventListener('wheel', findScrollDirectionOtherBrowsers);
+
+        function findScrollDirectionOtherBrowsers(event) {
+            var scrollDifference = scrollHeight - slideSize - acs.scrollTop;
+
+                            // Scroll wheel browser compatibility
+                            var delta = event.wheelDelta || -1 * event.deltaY;
+            
+            // Enable scrolling if at edges
+            var spos = delta < 0 ? 0 : scrollDifferenceTop;
+            
+            if (!(scrollDifference == spos))
+                mainSlider.mousewheel.disable();
+            else
+                mainSlider.mousewheel.enable();
         }
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
     }
-    
+});
+
+
+
+$('.slider .slider__wrapper').each(function () {
+    $(this).slick({
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: false,
+        dots:true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        fade: true,
+        customPaging: function(slick,index) {
+            
+            return '<span>' + (index + 1) + '</span>';
+        }
+    })
+
+    if($(this).find('.slider__item').length <= 1) {
+        $(this).find('.slick-dots').hide();
+        $(this).find('.slider__pag').hide();
+    } else {
+        $(this).siblings('.slider__pag').find('.button-prev').click(function() {
+            console.log($(this))
+            $(this).closest('.slider').find('.slider__wrapper').slick('slickPrev');
+        })
+        $(this).siblings('.slider__pag').find('.button-next').click(function() {
+            $(this).closest('.slider').find('.slider__wrapper').slick('slickNext');
+        })
+    }
+
 });
